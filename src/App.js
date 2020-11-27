e9922e77dd71a4098599392a0c9bc8f579c8e27d
Khalem <33363455+Khalem@users.noreply.github.com>
2020-11-27 15:30:05 +0000

    added date and time to weather card

--- src/App.js
+++ src/App.js
@@ -18,25 +18,45 @@ class App extends React.Component {
     this.months = ['January', 'Febuarary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     this.state = {
-      date: {},
+      date: new Date(),
       lat: 0,
       lon: 0,
       location: [],
       dailyTemps: [],
-      hourlyTemps: []
+      hourlyTemps: [],
+      time: ''
     }
   }
 
   componentDidMount() {
-    let d = new Date();
     navigator.geolocation.getCurrentPosition(position => {
       const lat = position.coords.latitude;
       const lon = position.coords.longitude;
-      this.setState({ date: d.getDate(), lat, lon });
+      this.setState({ lat, lon });
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
       this.getLocationData(url);
       this.getHourlyData();
+
+      this.intervalID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  tick() {
+    const d = new Date();
+    const hours = d.getHours() < 10 ? `0${d.getHours()}` : d.getHours();
+    const minutes = d.getMinutes() < 10 ? `0${d.getMinutes()}` : d.getMinutes();
+    const seconds = d.getSeconds() < 10 ? `0${d.getSeconds()}` : d.getSeconds();
+    
+    this.setState({
+      time: `${hours}:${minutes}:${seconds}`
     });
   }
 
@@ -84,7 +104,7 @@ class App extends React.Component {
       let time = item.dt - hourData.timezone_offset;
       let d = new Date(time*1000);
 
-      if (d.getDate() == this.state.date ) {
+      if (d.getDate() === this.state.date.getDate() ) {
         let hour = d.getHours();
         let minutes = d.getMinutes();
         let name = `${hour}:${minutes}0`;
@@ -101,14 +121,26 @@ class App extends React.Component {
       }
     });
 
-    this.setState({ dailyTemps: hourData.daily, hourlyTemps });
+    let location = this.state.location;
+    const currentDate = new Date();
+    const differenceTime = Math.abs(this.state.date - currentDate);
+    const difference = Math.ceil(differenceTime / (1000 * 60 * 60 * 24 ));
+    if (this.state.date.getDate() !== currentDate.getDate()) {
+      location.temp = Math.round(hourData.daily[difference].temp.day - 273.15);
+      location.description = hourData.daily[difference].weather[0].description;
+      location.icon = hourData.daily[difference].weather[0].icon;
+    } else {
+      location.temp = Math.round(hourData.daily[0].temp.day - 273.15);
+      location.description = hourData.daily[0].weather[0].description;
+      location.icon = hourData.daily[0].weather[0].icon;
+    }
+
+    this.setState({ location, dailyTemps: hourData.daily, hourlyTemps });
   }
 
-
   handleDailyClick = date => {
-    this.setState({ date: date.getDate() });
+    this.setState({ date });
     this.getHourlyData();
-
   }
 
   render() {
@@ -120,6 +152,10 @@ class App extends React.Component {
             location={this.state.location} 
             icon={ICONS[this.state.location.icon]} 
             hourlyTemps={this.state.hourlyTemps}
+            date={this.state.date}
+            time={this.state.time}
+            days={this.days} 
+            months={this.months} 
           />
           : <h1>Loading..</h1>
         }
--- src/components/weather-card/weather-card.component.jsx
+++ src/components/weather-card/weather-card.component.jsx
@@ -3,7 +3,7 @@ import { AreaChart, Area, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'rec
 
 import './weather-card.styles.scss';
 
-const WeatherCard = ({ location, icon, hourlyTemps }) => {
+const WeatherCard = ({ location, icon, hourlyTemps, date, time, days, months }) => {
     const tooltipStyles = {
         width: '100%',
         height: '100%',
@@ -14,12 +14,22 @@ const WeatherCard = ({ location, icon, hourlyTemps }) => {
         boxShadow: '0px 2px 10px 3px rgba(0,0,0,0.25)'
     };
 
+    const d = new Date();
+    time = d.getDate() != date.getDate() ? '' : time;
+    const dateStr = `${days[date.getDay()]} ${date.getDate()} ${months[date.getMonth()]}`;
+
     return (
         <div className='weather-card'>
             <div className='weather-card-header'>
-                <h1 className='card-title'>{location.name} {icon} <span className='temp'>{location.temp}°C</span></h1>
-                <p>{location.description}</p>
-                <h2 className='card-country'>{location.country}</h2>
+                <h1 className='card-title'>{location.temp}°</h1>
+                <div className='header-location-date'>
+                    <h2>{location.name}</h2>
+                    <p>{time} {dateStr}</p>
+                </div>
+                <div className='header-weather-desc'>
+                    {icon}
+                    <p>{location.description}</p>
+                </div>
             </div>
             <div className='weather-card-body'>
                 <h2>Hourly Data</h2>
@@ -41,7 +51,7 @@ const WeatherCard = ({ location, icon, hourlyTemps }) => {
                         </AreaChart>
                     </ResponsiveContainer>
                     :
-                    <h1>We're sorry, there is no hourly data available</h1>
+                    <h1>We're sorry, there is no hourly data available for {dateStr}</h1>
                 }
             </div>
         </div>
